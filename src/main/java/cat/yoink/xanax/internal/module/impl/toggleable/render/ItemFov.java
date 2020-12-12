16fe834c5de45c2b9f0951d784e4f91df0e0ab46
Katatje <geertspruijt6@outlook.com>
2020-12-12 15:39:15 +0800

    Fix package name and imports

--- src/main/java/cat/yoink/xanax/internal/module/impl/toggleable/render/ItemFov.java
+++ src/main/java/cat/yoink/xanax/internal/module/impl/toggleable/render/ItemFov.java
@@ -1,18 +1,11 @@
-package cat.yoink.xanax.internal.module.impl.toggleable.world;
+package cat.yoink.xanax.internal.module.impl.toggleable.render;
 
-import cat.yoink.xanax.internal.event.impl.CollisionEvent;
-import cat.yoink.xanax.internal.event.impl.WaterPushEvent;
 import cat.yoink.xanax.internal.module.ModuleCategory;
 import cat.yoink.xanax.internal.module.main.ModuleData;
 import cat.yoink.xanax.internal.module.state.StateModule;
-import cat.yoink.xanax.internal.setting.types.ListSetting;
 import cat.yoink.xanax.internal.setting.types.NumberSetting;
-import cat.yoink.xanax.internal.setting.types.StateSetting;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import javax.swing.plaf.nimbus.State;
 
 @ModuleData(name = "ItemFov", category = ModuleCategory.RENDER, description = "Change your fov excluding the item location")
 public final class ItemFov extends StateModule
