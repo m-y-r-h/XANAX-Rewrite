7e7779db6aa23fea2893bdeb0ad2113d7653eb49
Katatje <geertspruijt6@outlook.com>
2020-12-13 19:30:55 +0800

    ColorSetting

---
+++ src/main/java/cat/yoink/xanax/internal/setting/types/ColorSetting.java
@@ -0,0 +1,35 @@
+package cat.yoink.xanax.internal.setting.types;
+
+import cat.yoink.xanax.internal.setting.Setting;
+
+import java.awt.*;
+import java.util.function.Predicate;
+
+public final class ColorSetting extends Setting<Color>
+{
+    private Color value;
+
+    public ColorSetting(String name, Color value)
+    {
+        super(name);
+        this.value = value;
+    }
+
+    public ColorSetting(String name, Predicate<Setting<Color>> visible, Color value)
+    {
+        super(name, visible);
+        this.value = value;
+    }
+
+    @Override
+    public Color getValue()
+    {
+        return value;
+    }
+
+    @Override
+    public void setValue(Color value)
+    {
+        this.value = value;
+    }
+}
