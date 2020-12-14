8a9aa8c0644449ff01cbc33003e54ff34aef4f54
Katatje <geertspruijt6@outlook.com>
2020-12-14 11:02:25 +0800

    lambda and hole option in AutoCreeper

--- src/main/java/cat/yoink/xanax/internal/module/impl/toggleable/combat/AutoCreeper.java
+++ src/main/java/cat/yoink/xanax/internal/module/impl/toggleable/combat/AutoCreeper.java
@@ -3,9 +3,10 @@ package cat.yoink.xanax.internal.module.impl.toggleable.combat;
 import cat.yoink.xanax.internal.module.ModuleCategory;
 import cat.yoink.xanax.internal.module.main.ModuleData;
 import cat.yoink.xanax.internal.module.state.StateModule;
+import cat.yoink.xanax.internal.setting.types.ListSetting;
 import cat.yoink.xanax.internal.setting.types.NumberSetting;
 import cat.yoink.xanax.internal.util.InventoryUtil;
-import net.minecraft.entity.player.EntityPlayer;
+import cat.yoink.xanax.internal.util.WorldUtil;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.network.play.client.CPacketPlayerTryUseItemOnBlock;
@@ -21,8 +22,9 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 @ModuleData(name = "AutoCreeper", category = ModuleCategory.COMBAT, description = "wtf is this please help")
 public final class AutoCreeper extends StateModule
 {
+    private final ListSetting mode = addSetting(new ListSetting("Mode", "Hole", "Hole", "Always"));
     private final NumberSetting distance = addSetting(new NumberSetting("Distance", 4, 2, 7, 0.1));
-    private final NumberSetting delay = addSetting(new NumberSetting("Delay", 3, 0, 20, 1));
+    private final NumberSetting delay = addSetting(new NumberSetting("Delay", 3, 1, 20, 1));
     private int oldSlot;
 
     @Override
@@ -41,22 +43,19 @@ public final class AutoCreeper extends StateModule
     @SubscribeEvent
     public void onTickClientTick(TickEvent.ClientTickEvent event)
     {
-        if (isSafe())
+        if (isSafe() && mc.player.ticksExisted % delay.getValue() == 0)
         {
-            if (mc.player.ticksExisted % delay.getValue() + 1 != 1) return;
-            for (EntityPlayer player : mc.world.playerEntities)
-            {
-                if (mc.player.getDistance(player) <= distance.getValue() && !player.equals(mc.player))
-                {
-                    BlockPos blockPos = new BlockPos(player.posX, player.posY - 1, player.posZ);
-                    if (!mc.world.getBlockState(blockPos).getBlock().equals(Blocks.AIR))
-                    {
+            mc.world.playerEntities.stream()
+                    .filter(player -> mc.player.getDistance(player) <= distance.getValue())
+                    .filter(player -> !player.equals(mc.player))
+                    .filter(player -> mode.getValue().equals("Always") || WorldUtil.isInHole(player))
+                    .forEach(player -> {
+                        BlockPos blockPos = new BlockPos(player.posX, player.posY - 1, player.posZ);
+                        if (mc.world.getBlockState(blockPos).getBlock().equals(Blocks.AIR))return;
                         switchSlot();
                         mc.player.connection.sendPacket(new CPacketPlayerTryUseItemOnBlock(blockPos, EnumFacing.UP, EnumHand.MAIN_HAND, blockPos.getX(), blockPos.getY(), blockPos.getZ()));
                         mc.player.swingArm(EnumHand.MAIN_HAND);
-                    }
-                }
-            }
+                    });
         }
     }
 
