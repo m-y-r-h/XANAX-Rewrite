c9a2aa0216af0981a37f92df5f6e755944d450c8
Katatje <geertspruijt6@outlook.com>
2020-12-27 23:38:43 +0800

    WebUtil

---
+++ src/main/java/cat/yoink/xanax/internal/util/WebUtil.java
@@ -0,0 +1,28 @@
+package cat.yoink.xanax.internal.util;
+
+import cat.yoink.xanax.internal.traits.interfaces.Minecraft;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * @author yoink
+ */
+public final class WebUtil implements Minecraft
+{
+    public static String getContent(String url)
+    {
+        try
+        {
+            StringBuilder content = new StringBuilder();
+            URL url1 = new URL(url);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url1.openStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) content.append(line).append("\n");
+            return content.toString();
+        }
+        catch (Exception ignored) {  }
+        return "";
+    }
+}
