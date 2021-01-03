62750aa8e3b27e449cf1522d76165b4788827368
Katatje <geertspruijt6@outlook.com>
2021-01-03 18:32:08 +0800

    Update CapeManager

--- src/main/java/cat/yoink/xanax/internal/feature/other/CapeManager.java
+++ src/main/java/cat/yoink/xanax/internal/feature/other/CapeManager.java
@@ -14,9 +14,8 @@ public final class CapeManager extends MapRegistry<String, ResourceLocation>
     public static final CapeManager INSTANCE = new CapeManager("https://raw.githubusercontent.com/Katatje/XANAX-Rewrite/master/capes.txt");
 
     private CapeManager(String url)
-    { // TODO: 12/27/2020 Make this customizable. When i'm back home O_O
-        ResourceLocation cape = new ResourceLocation("cape.png");
+    {
         String content = WebUtil.getContent(url);
-        Arrays.stream(content.split("\n")).forEach(s -> register(s, cape));
+        Arrays.stream(content.split("\n")).map(s -> s.split(":")).forEach(strings -> register(strings[0], new ResourceLocation(strings[1])));
     }
 }
